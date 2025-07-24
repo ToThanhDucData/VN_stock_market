e5e83bb9a61d9767168c35100c1732e3ea296d97
To Thanh Duc <tothanhducdata@gmail.com>
2025-07-24 20:36:31 +0700

    Added extract_information func to web_api_caller.py

--- code/core/web_api_caller.py
+++ code/core/web_api_caller.py
@@ -17,3 +17,27 @@ def get_API(ticker, timestamp, days):
     
     api_url = f'https://{sub_domain}.{domain}/{api_type}?{p1}&{p2}&{p3}&{p4}&{p5}'
     return api_url
+
+def extract_information(raw_json):
+    print("\tFormatting data from JSON..")
+    raw_json['data'] = raw_json['data'].apply(lambda x: re.sub("[{ }]", "", x).split(","))
+    print('\t-----------------------------------------------------')
+
+    to_extract = ['open', 'high', 'low', 'close', 'volume', 'date']
+    col_index = 0
+    for col in to_extract:
+        print("\tExtracting column {}..".format(col.upper()))
+        raw_json[col] = raw_json['data'].apply(lambda x: x[col_index].split(":")[1]).replace("None", np.nan)
+        col_index += 1
+    print('\t-----------------------------------------------------')
+
+    to_number = ['open', 'high', 'low', 'close', 'volume']
+    for col in to_number:
+        print("\tChanging data type for column {}..".format(col.upper()))
+        raw_json[col] = raw_json[col].astype(float)
+
+    print("\tChanging data type for column DATE")
+    raw_json['date'] = pd.to_datetime(raw_json['date'].apply(lambda x: x.split("T")[0].replace("'", "")))
+    print('\t-----------------------------------------------------')
+
+    return raw_json[['ticker','open', 'high', 'low', 'close', 'volume', 'date']]
