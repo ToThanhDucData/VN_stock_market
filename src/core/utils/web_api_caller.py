e2a345e9d9f02b13cd95280a6db4b2f94d142fe0
To Thanh Duc <tothanhducdata@gmail.com>
2025-07-26 10:26:36 +0700

    Added timestamp to historical data

--- src/core/utils/web_api_caller.py
+++ src/core/utils/web_api_caller.py
@@ -26,13 +26,13 @@ def get_API(ticker, timestamp, days):
     raw_df = pd.read_json(api_url)
     return raw_df
 
-def get_stock_historical_price(ticker_name, current_timestamp):
+def get_stock_historical_price(ticker_name, current_timestamp, count_back):
     raw = pd.DataFrame()
     while True:
         date_to_get = str(datetime.fromtimestamp(current_timestamp))[:10]
-        print(f'All 365 days historical prices of ticker {ticker_name} before {date_to_get}')
+        print(f'All {count_back} days historical prices of ticker {ticker_name} before {date_to_get}')
         
-        raw_df = get_API(ticker = ticker_name, timestamp = current_timestamp, days = 365)
+        raw_df = get_API(ticker = ticker_name, timestamp = current_timestamp, days = count_back)
         raw = pd.concat([raw, raw_df])
         
         if raw_df.shape[0] == 0:
@@ -42,14 +42,17 @@ def get_stock_historical_price(ticker_name, current_timestamp):
         sleep(0.5)
     return raw
 
-def get_all_stocks_historical_price(stocks_list, processing_ref, processing_df):
-    print("Reading from PROCESSING INDEX file..")
-    __stock_epoch = incremental.incremental_index(saved_file = processing_ref)
+def get_all_stocks_historical_price(stocks_list, incremental_index_file, processing_df):
+    print("Reading from INCREMENTAL INDEX file..")
+    __stock_epoch = incremental.incremental_index(saved_file = incremental_index_file)
 
+    #---------------------------------------------------------------
     if __stock_epoch == 0:
         with open(processing_df, 'a') as raw_f:
-            raw_f.write("ticker,data,dataT1\n")
+            raw_f.write("ticker,data,last_updated\n")
+    #---------------------------------------------------------------
 
+    #---------------------------------------------------------------
     __all_stocks_number = len(stocks_list)
     for stock in stocks_list[__stock_epoch:]:
         print('')
@@ -57,14 +60,14 @@ def get_all_stocks_historical_price(stocks_list, processing_ref, processing_df):
         print('=====================================================')
         
         current_timestamp = int(time())
-        _raw = get_stock_historical_price(ticker_name = stock, current_timestamp = current_timestamp)
-        _raw.fillna('', inplace = True)
+        _raw = get_stock_historical_price(ticker_name = stock, current_timestamp = current_timestamp, count_back = 365)
+        #_raw.fillna('', inplace = True)
 
         print('-----------------------------------------------------')
         print("Updating PROCESSING DATA..")
         with open(processing_df, 'a') as raw_f:
             for _, row in _raw.iterrows():
-                raw_f.write(row.ticker + ",\"" + str(row.data) + "\"," + row.dataT1 + '\n')
+                raw_f.write(row.ticker + ",\"" + str(row.data) + "\"," + str(time()) + '\n')
         print("---PROCESSING DATA UPDATED.---")
         print('-----------------------------------------------------')
         __stock_epoch += 1
@@ -72,13 +75,14 @@ def get_all_stocks_historical_price(stocks_list, processing_ref, processing_df):
         print('{:,}/{:,}'.format(__stock_epoch, __all_stocks_number))
 
         print('-----------------------------------------------------')
-        print("Updating PROCESSING INDEX..")
-        with open (processing_ref, 'a') as index_f:
+        print("Updating INCREMENTAL INDEX..")
+        with open (incremental_index_file, 'a') as index_f:
             index_f.write(str(__stock_epoch) + '\n')
-        print("---PROCESS UPDATED.---")
+        print("---INCREMENTAL INDEX UPDATED.---")
         print('-----------------------------------------------------')
         print('=====================================================')
         print('')
+    #---------------------------------------------------------------
 
     return pd.read_csv(processing_df)
 
@@ -104,4 +108,4 @@ def extract_information(raw_json):
     raw_json['date'] = pd.to_datetime(raw_json['date'].apply(lambda x: x.split("T")[0].replace("'", "")))
     print('\t-----------------------------------------------------')
 
-    return raw_json[['ticker','open', 'high', 'low', 'close', 'volume', 'date']]
+    return raw_json[['ticker','open', 'high', 'low', 'close', 'volume', 'date', 'last_updated']]
