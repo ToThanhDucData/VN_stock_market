c0012933b942971a0be5601df7e7461c5c0add7b
To Thanh Duc <tothanhducdata@gmail.com>
2025-07-27 09:48:37 +0700

    Modified error_log.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-import os
 from utils.get_all_tickers_history import get_all_tickers_history
 from utils.error_log import errors_log
 
@@ -7,12 +6,7 @@ def main():
     get_all_tickers_history()
 
 if __name__ == '__main__':
-    #---------------------------------------------------------------
-    ERROR_PATH = os.getenv('ERROR_PATH')
-    ERROR = os.path.join(ERROR_PATH, os.getenv('ERROR_FILE'))
-    #---------------------------------------------------------------
-
     try:
         main()
     except Exception as e:
-        errors_log(ERROR, e)
+        errors_log(e)
--- src/utils/error_log.py
+++ src/utils/error_log.py
@@ -1,6 +1,12 @@
 from time import localtime, strftime
+import os
 
-def errors_log(error_file, e):
+def errors_log(e):
+    #---------------------------------------------------------------
+    ERROR_PATH = os.getenv('ERROR_PATH')
+    ERROR = os.path.join(ERROR_PATH, os.getenv('ERROR_FILE'))
+    #---------------------------------------------------------------
+
     curr_time = strftime("%Y:%m:%d %H:%M:%S", localtime())
-    with open(error_file, 'a') as f:
+    with open(ERROR, 'a') as f:
         f.write(f'[{curr_time}] --- {e} \n')
