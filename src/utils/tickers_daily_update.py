ac1c717b3c4d001456f85b130dba0e49d3de47bf
To Thanh Duc <tothanhducdata@gmail.com>
2025-07-28 18:43:30 +0700

    Modified tickers_daily_update.py and .env

--- src/utils/tickers_daily_update.py
+++ src/utils/tickers_daily_update.py
@@ -12,14 +12,14 @@ def get_all_tickers_history(test:bool = False) -> None:
 
     test_env = [
         'PROCESSING_DATA_TEST_FILE'
-        ,'ALL_HIST_INCREMENTAL_INDEX_TEST_FILE'
+        ,'DAILY_INCREMENTAL_INDEX_TEST_FILE'
         ,'ALL_STOCKS_TEST_FILE'
         ,'STOCK_HISTORICAL_PRICE_TEST_FILE'
     ]
 
     prod_env = [
         'PROCESSING_DATA_FILE'
-        ,'ALL_HIST_INCREMENTAL_INDEX_FILE'
+        ,'DAILY_INCREMENTAL_INDEX_FILE'
         ,'ALL_STOCKS_FILE'
         ,'STOCK_HISTORICAL_PRICE_FILE'
     ]
@@ -33,7 +33,7 @@ def get_all_tickers_history(test:bool = False) -> None:
 
     #---------------------------------------------------------------
     INCREMENTAL_INDEX_PATH = os.getenv('INCREMENTAL_INDEX_PATH')
-    ALL_HIST_INCREMENTAL_INDEX = os.path.join(INCREMENTAL_INDEX_PATH, os.getenv(to_get_path[1]))
+    DAILY_INCREMENTAL_INDEX = os.path.join(INCREMENTAL_INDEX_PATH, os.getenv(to_get_path[1]))
     #---------------------------------------------------------------
 
     #---------------------------------------------------------------
@@ -48,7 +48,7 @@ def get_all_tickers_history(test:bool = False) -> None:
     stocks_list = stocks['ticker_id'].to_list()
     
     print("Calling API...")
-    latest_df = get_all_stocks_historical_price(stocks_list, ALL_HIST_INCREMENTAL_INDEX, PROCESSING_DATA, test)
+    latest_df = get_all_stocks_historical_price(stocks_list, DAILY_INCREMENTAL_INDEX, PROCESSING_DATA, test)
 
     print("Formatting to tabular...")
     latest_extracted = extract_information(latest_df)
