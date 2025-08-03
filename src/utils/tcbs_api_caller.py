7f4776bd1beb6721f136d6c860d47c795f0a6c5f
To Thanh Duc <tothanhducdata@gmail.com>
2025-08-03 08:34:46 +0700

    Refactored tcbs_api_caller.py

--- src/utils/tcbs_api_caller.py
+++ src/utils/tcbs_api_caller.py
@@ -5,39 +5,60 @@ from utils.dynamic_name import add_text
 from time import time, sleep
 from datetime import datetime
 
+def get_stock_historical_price_count_back_none(ticker_name:str
+                                               ,current_timestamp:int
+                                               ) -> pd.DataFrame:
+    raw = pd.DataFrame()
+    while True:
+        date_to_get = str(datetime.fromtimestamp(current_timestamp))[:10]
+        print(f'Getting 1 year of historical prices of ticker {ticker_name} before {date_to_get}')
+        
+        raw_df = get_TCBS_API(ticker = ticker_name, timestamp = current_timestamp, days = 365)
+        raw = pd.concat([raw, raw_df])
+        
+        if raw_df.shape[0] == 0:
+            break
+
+        current_timestamp -= 365*24*60*60
+        sleep(0.5)
+
+    return raw
+
+def get_stock_historical_price_count_back_not_none(ticker_name:str
+                                                   ,current_timestamp:int
+                                                   ,count_back:int
+                                                   ) -> pd.DataFrame:
+    raw = pd.DataFrame()
+    while raw.shape[0] < count_back:
+        date_to_get = str(datetime.fromtimestamp(current_timestamp))[:10]
+        print(f'Getting 1 year of historical prices of ticker {ticker_name} before {date_to_get}')
+        
+        raw_df = get_TCBS_API(ticker = ticker_name, timestamp = current_timestamp, days = 365)
+        raw = pd.concat([raw, raw_df])
+
+        current_timestamp -= 365*24*60*60
+        sleep(0.5)
+    raw['date'] = raw['data'].apply(lambda x: pd.to_datetime(x['tradingDate']))
+    raw = raw.sort_values(by = 'date', ascending = False).head(count_back)
+
+    return raw
+
 def get_stock_historical_price(ticker_name:str
                                ,current_timestamp:int
                                ,count_back:int = None
                                ) -> pd.DataFrame:
-    raw = pd.DataFrame()
     if count_back == None:
-        while True:
-            date_to_get = str(datetime.fromtimestamp(current_timestamp))[:10]
-            print(f'Getting 1 year of historical prices of ticker {ticker_name} before {date_to_get}')
-            
-            raw_df = get_TCBS_API(ticker = ticker_name, timestamp = current_timestamp, days = 365)
-            raw = pd.concat([raw, raw_df])
-            
-            if raw_df.shape[0] == 0:
-                break
-
-            current_timestamp -= 365*24*60*60
-            sleep(0.5)
+        raw = get_stock_historical_price_count_back_none(ticker_name=ticker_name
+                                                         ,current_timestamp=current_timestamp)
     else:
         if count_back < 365:
-            raw = get_TCBS_API(ticker = ticker_name, timestamp = current_timestamp, days = count_back)
+            raw = get_TCBS_API(ticker = ticker_name
+                               ,timestamp = current_timestamp
+                               ,days = count_back)
         else:
-            while raw.shape[0] < count_back:
-                date_to_get = str(datetime.fromtimestamp(current_timestamp))[:10]
-                print(f'Getting 1 year of historical prices of ticker {ticker_name} before {date_to_get}')
-                
-                raw_df = get_TCBS_API(ticker = ticker_name, timestamp = current_timestamp, days = 365)
-                raw = pd.concat([raw, raw_df])
-
-                current_timestamp -= 365*24*60*60
-                sleep(0.5)
-            raw['date'] = raw['data'].apply(lambda x: pd.to_datetime(x['tradingDate']))
-            raw = raw.sort_values(by = 'date', ascending = False).head(count_back)
+            raw = get_stock_historical_price_count_back_not_none(ticker_name=ticker_name
+                                                                 ,current_timestamp=current_timestamp
+                                                                 ,count_back=count_back)
     return raw
 
 def get_all_stocks_historical_price(stocks_list:list
